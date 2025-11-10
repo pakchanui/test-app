2de01abae1daee660fcff4f505ec5cd7d1d7d0f8
chanui <saesoon920@handong.ac.kr>
2025-11-10 10:40:13 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Study!!! <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
